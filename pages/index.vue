6666594bf046350ab89e459229a1bfe4b33df134
y4wee <quentin.jambert@gmail.com>
2023-02-02 16:51:45 +0100

    search function

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,31 @@
 <script setup>
 const dataCandidacy = useDataCandidacy();
 
-onMounted(() => {});
+const sortedCandidacy = ref("");
+
+onMounted(() => {
+    console.log(sortedCandidacy.value);
+    console.log();
+});
 </script>
 
 <template>
     <div class="home">
         <Header />
         <FormCandidacy />
+        <input type="text" v-model="sortedCandidacy" />
         <div class="candidacyContainer">
             <CardCandidacy
                 v-for="candidacy in dataCandidacy"
                 :key="candidacy.id"
                 :candidacy="candidacy"
+                v-show="
+                    sortedCandidacy
+                        ? candidacy.name
+                              .toLowerCase()
+                              .startsWith(sortedCandidacy.toLowerCase())
+                        : true
+                "
             />
         </div>
     </div>
@@ -30,11 +43,11 @@ $colorRed: #ff5959;
     display: flex;
     flex-direction: column;
     width: 100vw;
+    padding-top: 100px;
 }
 .candidacyContainer {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
-    padding-top: 100px;
 }
 </style>
